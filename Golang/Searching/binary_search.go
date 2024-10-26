0b66e4568cc7b34f8cd7bdcf254250d559b2d66f
solace06 <khushi.sharma061101@gmail.com>
2024-10-26 21:45:57 +0530

    Binary Search

--- Golang/Searching/binary_search.go
+++ Golang/Searching/binary_search.go
@@ -1,1 +1,28 @@
 package main
+
+import "fmt"
+
+func binarySearch(arr []int, size int) {
+	
+	var element int
+	fmt.Println("Enter the element to be searched for")
+	fmt.Scan(&element)
+
+	low:=0
+	high:=size-1
+
+	for low<=high{
+		mid:=(low+high)/2
+		if arr[mid]==element{
+			fmt.Printf("Found the element at %d",mid)
+			return
+		} else if arr[mid]>element{
+			high=mid-1
+		} else{
+			low=mid+1
+		}
+	}
+
+	fmt.Println("The element is not in the list")
+	return
+}
