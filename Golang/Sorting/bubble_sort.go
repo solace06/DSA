cfefebe09544760c247743ada269411296f98953
solace06 <khushi.sharma061101@gmail.com>
2024-10-27 23:48:10 +0530

    "Bubble Sort"

--- Golang/Sorting/bubble_sort.go
+++ Golang/Sorting/bubble_sort.go
@@ -1,1 +1,21 @@
 package main
+
+import "reflect"
+
+func bubbleSort(arr []int, size int) {
+
+	var swapped int
+	swap := reflect.Swapper(arr)
+	for i := 0; i < size-1; i++ {
+		swapped = 0
+		for j := 0; j < size-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				swap(j,j+1)
+				swapped=1
+			}
+		}
+		if swapped==0{
+			break;
+		}
+	}
+}
--- Golang/Sorting/main.go
+++ Golang/Sorting/main.go
@@ -1,1 +1,40 @@
 package main
+
+import "fmt"
+
+func printArray(arr []int, size int){
+	for i:=0; i<size; i++{
+		fmt.Print(arr[i]," ")
+	}
+	fmt.Print("\n")
+}
+
+func main() {
+
+	var size int
+	fmt.Println("Enter the size of the array")
+	fmt.Scan(&size)
+
+	arr:=make([]int,size)
+
+	fmt.Println("Enter ",size," elements")
+	for i:=0; i<size; i++{
+		fmt.Scan(&arr[i])
+	}
+
+	fmt.Println("Original Array")
+	printArray(arr,size)
+
+	var sort string
+	fmt.Println("Select the type of sort you want to execute")
+	fmt.Println("1.Select A for Bubble Sort")
+	fmt.Scan(&sort)
+	switch sort{
+	case "A":
+		fmt.Println("Starting Bubble Sort")
+		bubbleSort(arr, size)
+		fmt.Println("Sorted Array")
+		printArray(arr,size)
+	}
+
+}
