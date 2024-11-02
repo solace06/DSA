038b93faad2d15412576499b1f7d941699a43636
solace06 <khushi.sharma061101@gmail.com>
2024-11-03 02:02:31 +0530

    Merge Sort

--- C++/Sorting/merge_sort.cpp
+++ C++/Sorting/merge_sort.cpp
@@ -1,11 +1,44 @@
 #include <iostream>
+#include<vector>
 using namespace std;
 
-void mergeSort(int* arr, int size){
+void merge(vector<int>& arr,int low, int mid, int high){
+    int i=low;
+    int j=mid+1;
+    vector<int> temp;
+    while(i<=mid && j<=high){
+        if(arr[i]<=arr[j]){
+            temp.push_back(arr[i]);
+            i++;
+        }
+        else{
+            temp.push_back(arr[j]);
+            j++;
+        }
+    }
+    while(j<=high){
+        temp.push_back(arr[j]);
+        j++;
+    }
+    while(i<=mid){
+        temp.push_back(arr[i]);
+        i++;
+    }
 
+    for(int i=low; i<=high; i++){
+        arr[i]=temp[i-low];
+    }
 }
 
-void printArray(int* arr, int size){
+void mergeSort(vector<int>& arr, int low, int high){
+    if(low==high)return;
+    int mid=(low+high)/2;
+    mergeSort(arr,low,mid);
+    mergeSort(arr,mid+1,high);
+    merge(arr,low,mid,high);
+}
+
+void printArray(vector<int>& arr, int size){
     for(int i=0; i<size; ++i){
         cout<<arr[i]<<" ";
     }
@@ -18,7 +51,7 @@ int main(){
     cout<<"Enter the size of the elements\n";
     cin>>size;
 
-    int* arr= new int[size];
+    vector<int> arr(size);
 
     cout<<"Enter "<<size<<" elements\n";
     for(int i=0; i<size; i++){
@@ -28,7 +61,7 @@ int main(){
     cout<<"Original Array\n";
     printArray(arr, size);
 
-    mergeSort(arr, size);
+    mergeSort(arr, 0, size-1);
     cout<<endl;
 
     cout<<"Sorted Array\n";
