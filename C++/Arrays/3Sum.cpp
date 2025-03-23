07d8ed38ccb82cd6908e03a69cc77730d33daa3e
solace06 <khushi.sharma061101@gmail.com>
2025-03-23 14:27:17 +0530

    better approach for 3 sum

--- C++/Arrays/3Sum.cpp
+++ C++/Arrays/3Sum.cpp
@@ -6,6 +6,8 @@ using namespace std;
 int main() {
     int arr[6]={-1,0,1,2,-1,-4};
     set <vector<int>> ans;
+    
+    //brute force approach
     for(int i=0; i<6; i++){
         for(int j=i+1; j<6; j++){
             for(int k=j+1; k<6; k++){
@@ -15,6 +17,20 @@ int main() {
                     ans.insert(temp);
                 }
             }
+        }
+    }
+
+    //better approach
+    for(int i=0; i<6; i++){
+        set <int> hashset;
+        for(int j=i+1; j<6; j++){
+            int third=-(arr[i]+arr[j]);
+            if(hashset.find(third)!=hashset.end()){
+                vector<int> temp={arr[i],arr[j],third};
+                sort(temp.begin(), temp.end());
+                ans.insert(temp);
+            }
+            hashset.insert(arr[j]);
         }
     }
     
