d23b09b484cb8c796a0b8dc9aed6a5c4a1a14fc5
solace06 <khushi.sharma061101@gmail.com>
2025-05-18 23:47:52 +0530

    delete the node before value

--- Linked List/delete.go
+++ Linked List/delete.go
@@ -5,53 +5,58 @@ import (
 	"fmt"
 )
 
-func deleteFront(head *Node) *Node{
-	if head==nil{
+func deleteFront(head *Node) *Node {
+	if head == nil {
 		fmt.Println("Linked List is empty")
 		return nil
 	}
-	
+
 	fmt.Println("Deleted Node: ", head.data)
-	head=head.next
+	head = head.next
 	return head
 }
 
-func deleteBack(head *Node) (*Node, error){
-	if head == nil{
+func deleteBack(head *Node) (*Node, error) {
+	if head == nil {
 		return nil, errors.New("the linked list is empty")
 	}
 
-	if head.next==nil{
+	if head.next == nil {
 		return nil, nil
 	}
 
-	curr:=head
-	for curr.next.next!=nil{
-		curr=curr.next
+	curr := head
+	for curr.next.next != nil {
+		curr = curr.next
 	}
-	curr.next=nil
+	curr.next = nil
 	fmt.Println("The last node has been deleted successfully")
 	return head, nil
 }
 
-func deleteAfter(head *Node, value int) error{
-	curr:=head
+func deleteAfter(head *Node, value int) error {
+	curr := head
 
-	for curr.data!=value && curr != nil{
-		curr=curr.next
+	for curr.data != value && curr != nil {
+		curr = curr.next
 	}
 
-	if curr == nil{
-		return fmt.Errorf("node with value %d does not exist",value)
+	if curr == nil {
+		return fmt.Errorf("node with value %d does not exist", value)
 	}
 
-	if curr.next == nil{
-		return fmt.Errorf("node with value %d is the last node",value)
+	if curr.next == nil {
+		return fmt.Errorf("node with value %d is the last node", value)
 	}
 
 	var temp *Node = curr.next
-	fmt.Printf("node after value %d and with data %d is successfully deleted",value,temp.data)
+	fmt.Printf("node after value %d and with data %d is successfully deleted", value, temp.data)
 
-	curr.next=curr.next.next
+	curr.next = curr.next.next
+	return nil
+}
+
+func deleteBefore(head *Node, value int) error {
+
 	return nil
 }
