a6a16b3e95f1049526aab079a91857a28f22de5c
solace06 <khushi.sharma061101@gmail.com>
2025-05-20 10:42:37 +0530

    Select with Channels

---
+++ Golang/Concurrency/select.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "fmt"
+    "time"
+    )
+
+func selectChannel() {
+    // select with channels
+    
+    chan1:=make(chan string)
+    chan2:=make(chan string)
+    
+    go func(){
+       time.Sleep(2 * time.Second)
+       chan1<-"Message sent to channel 1"
+    }()
+    
+    go func(){
+        time.Sleep(4 * time.Second)
+        chan2<-"Message sent to channel 2"
+    }()
+    
+    for i:=0; i<2; i++{
+        select {
+            case val:=<-chan1:
+                fmt.Println(val)
+            case val:=<-chan2:
+                fmt.Println(val)
+        }
+    }
+}
