afce02c7eefece03b0f25813712ea44535a57ba3
solace06 <khushi.sharma061101@gmail.com>
2025-05-29 11:56:55 +0530

    finding second largest element

---
+++ Golang/Slices/secondlargest.go
@@ -0,0 +1,32 @@
+package slices
+
+import (
+    "fmt"
+    "sort"
+    )
+
+func secondlargest() {
+  var n int
+  fmt.Println("Enter the number of elements")
+  fmt.Scan(&n)
+  
+  fmt.Println("Enter the elements")
+  slice:=make([]int,n)
+  for i:=0; i<n; i++{
+      fmt.Scan(&slice[i])
+  }
+  
+  //brute force approach 
+  sort.Ints(slice)
+  largest:=slice[n-1]
+  second_largest:=-1
+  for i:=n-2; i>=0; i--{
+      if slice[i]!=largest{
+          second_largest=slice[i]
+          break
+      }
+  }
+  
+  fmt.Println("Second largest element is:",second_largest)
+  
+}
