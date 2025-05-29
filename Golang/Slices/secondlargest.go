75057f067bc326d7bbbc87b2d182288b0110d76d
solace06 <khushi.sharma061101@gmail.com>
2025-05-29 12:01:26 +0530

    better approach for second largest element

--- Golang/Slices/secondlargest.go
+++ Golang/Slices/secondlargest.go
@@ -1,32 +1,47 @@
 package slices
 
 import (
-    "fmt"
-    "sort"
-    )
+	"fmt"
+	"sort"
+)
 
 func secondlargest() {
-  var n int
-  fmt.Println("Enter the number of elements")
-  fmt.Scan(&n)
-  
-  fmt.Println("Enter the elements")
-  slice:=make([]int,n)
-  for i:=0; i<n; i++{
-      fmt.Scan(&slice[i])
-  }
-  
-  //brute force approach 
-  sort.Ints(slice)
-  largest:=slice[n-1]
-  second_largest:=-1
-  for i:=n-2; i>=0; i--{
-      if slice[i]!=largest{
-          second_largest=slice[i]
-          break
-      }
-  }
-  
-  fmt.Println("Second largest element is:",second_largest)
-  
+	var n int
+	fmt.Println("Enter the number of elements")
+	fmt.Scan(&n)
+
+	fmt.Println("Enter the elements")
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&slice[i])
+	}
+
+	//brute force approach
+	sort.Ints(slice)
+	largest := slice[n-1]
+	second_largest := -1
+	for i := n - 2; i >= 0; i-- {
+		if slice[i] != largest {
+			second_largest = slice[i]
+			break
+		}
+	}
+
+	//better approach
+	largest = slice[0]
+	for i := 0; i < n; i++ {
+		if slice[i] > largest {
+			largest = slice[i]
+		}
+	}
+
+	second_largest = -1
+	for i := 0; i < n; i++ {
+		if slice[i] > second_largest && largest > slice[i] {
+			second_largest = slice[i]
+		}
+	}
+
+	fmt.Println("Second largest element is:", second_largest)
+
 }
