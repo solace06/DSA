16b96448d3914ed693749677b6585f4e10612c09
solace06 <khushi.sharma061101@gmail.com>
2025-06-07 23:30:09 +0530

    safely accessing values via multiple goroutines with the help of mutex

---
+++ Golang/Concurrency/mutex.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+    "sync"
+    )
+    
+    
+func mute() {
+    var counter = 0
+    var wg sync.WaitGroup
+    var mu sync.Mutex
+    
+    for i:=0; i<100; i++{
+        wg.Add(1)
+        go func(){
+            defer wg.Done()
+            mu.Lock()
+            counter++
+            mu.Unlock()
+        }()
+    }
+        
+    wg.Wait()
+    fmt.Println("The value of the counter is:", counter)
+}
